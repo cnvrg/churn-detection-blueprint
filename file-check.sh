e87506a2edf82149ffe8a522e57092c734ba8472
Harshul-cnvrg <harshul.thakur@cnvrg.io>
2023-06-08 10:57:16 +0530

    file check

--- file-check.sh
+++ file-check.sh
@@ -2,7 +2,7 @@
 
 # Set the token patterns you want to search for
 TOKEN_PATTERN_1="\b[A-Za-z0-9]{32}\b"
-TOKEN_PATTERN_2='^[A-Za-z0-9+/]{20}==[A-Za-z0-9+/]{6}==$'
+#TOKEN_PATTERN_2='^[A-Za-z0-9+/]{20}==[A-Za-z0-9+/]{6}==$'
 
 # Get the commit hashes in reverse chronological order
 COMMITS=$(git rev-list --all)
@@ -17,7 +17,7 @@ for COMMIT in $COMMITS; do
     # Loop through each changed file
     for FILE in $FILES; do
         # Check if the file contains either token pattern
-        if grep -q -E "$TOKEN_PATTERN_1|$TOKEN_PATTERN_2" "$FILE"; then
+        if grep -q -E "$TOKEN_PATTERN_1" "$FILE"; then
             echo "Token pattern found in file: $FILE"
         fi
     done
